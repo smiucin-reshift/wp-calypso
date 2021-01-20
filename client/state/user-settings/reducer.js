8e7b73527bf1f7f51efd358bf8086cac80f44224
Markus Dobmann <m.do@posteo.net>
2021-01-20 12:27:17 +0100

    state(user-settings): use helpers to manage nested unsaved user settings

--- client/state/user-settings/reducer.js
+++ client/state/user-settings/reducer.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 
-import { omit } from 'lodash';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies
@@ -16,32 +16,37 @@ import {
 	USER_SETTINGS_SAVE_FAILURE,
 } from 'calypso/state/action-types';
 import { combineReducers } from 'calypso/state/utils';
+import { setValue, removeValue } from './helpers';
 
 export const settings = ( state = {}, { type, settingValues } ) =>
 	USER_SETTINGS_SAVE_SUCCCESS === type ? { ...state, ...settingValues } : state;
 
 export const unsavedSettings = ( state = {}, action ) => {
 	switch ( action.type ) {
-		case USER_SETTINGS_UNSAVED_CLEAR:
-			// After a successful save, remove the saved settings (either all of them,
-			// or a subset) from the `unsavedSettings`.
-			if ( ! action.settingNames ) {
+		case USER_SETTINGS_UNSAVED_CLEAR: {
+			const settingNames = action.settingNames;
+
+			if ( ! settingNames ) {
 				return {};
 			}
-			return omit( state, action.settingNames );
 
-		case USER_SETTINGS_UNSAVED_SET:
-			if ( state[ action.settingName ] === action.value ) {
+			if ( Array.isArray( settingNames ) ) {
+				return settingNames.reduce( removeValue, state );
+			}
+
+			return removeValue( state, settingNames );
+		}
+		case USER_SETTINGS_UNSAVED_SET: {
+			if ( get( state, action.settingName ) === action.value ) {
 				return state;
 			}
-			return { ...state, [ action.settingName ]: action.value };
-
-		case USER_SETTINGS_UNSAVED_REMOVE:
-			return omit( state, action.settingName );
-
-		default:
-			return state;
+			return setValue( state, action.settingName, action.value );
+		}
+		case USER_SETTINGS_UNSAVED_REMOVE: {
+			return removeValue( state, action.settingName );
+		}
 	}
+	return state;
 };
 
 export const updatingPassword = ( state = false, action ) => {
